001d941c55d62152fffc72931fbc9636b8994e80
adevaraz <zaravedam@gmail.com>
2023-02-09 13:59:43 +0700

    feat(api): add album apis

---
+++ 09-open-music-api/src/api/albums/handler.js
@@ -0,0 +1,152 @@
+/* eslint-disable no-underscore-dangle */
+const ClientError = require('../../exceptions/ClientError');
+
+class AlbumsHandler {
+  constructor(service) {
+    this._service = service;
+
+    this.postAlbumHandler = this.postAlbumHandler.bind(this);
+  }
+
+  async postAlbumHandler(request, h) {
+    try {
+      const { name = 'unknown', year } = request.payload;
+      const albumId = await this._service.addAlbum({ name, year });
+
+      const response = h.response({
+        status: 'success',
+        message: 'Album berhasil ditambahkan',
+        data: {
+          albumId,
+        },
+      });
+
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+
+        response.code(error.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async getAlbumsHandler() {
+    const albums = await this._service.getAlbums();
+    return {
+      status: 'success',
+      data: {
+        albums,
+      },
+    };
+  }
+
+  async getAlbumByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const album = await this._service.getAlbumById(id);
+
+      return {
+        status: 'success',
+        data: {
+          album,
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+
+        response.code(error.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async putAlbumByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+
+      await this._service.editAlbumById(id, request.payload);
+
+      return {
+        status: 'success',
+        message: 'Catatan berhasil diperbarui',
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+
+        response.code(error.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async deleteAlbumByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+
+      await this._service.deleteAlbumById(id);
+
+      return {
+        status: 'success',
+        message: 'Album berhasil dihapus',
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+
+        response.code(error.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+
+module.exports = AlbumsHandler;
---
+++ 09-open-music-api/src/server.js
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-extraneous-dependencies */
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const AlbumsService = require('./services/AlbumsService');
+// const AlbumsValidator = require('./validator/albums');
+
+const init = async () => {
+  const albumsService = new AlbumsService();
+
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumsService,
+      // validator: NotesValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
